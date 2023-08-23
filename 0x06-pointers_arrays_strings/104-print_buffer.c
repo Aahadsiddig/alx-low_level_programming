996f7109112cf71d124adae8a58f64db80bf045f
Aahadsiddig <aahadsiddigo@gmail.com>
2023-08-23 11:15:58 -0700

    done

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,10 +1,12 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
  * isPrintableASCII - determaines if n is a printable ASCII char
  * @n: integer
  * Return: 1 if true, 0 if false
 */
+
 int isPrintableASCII(int n)
 {
 	return (n >= 32 && n <= 126);
