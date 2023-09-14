c20f6f2f7e699c30b17e188a67364eadb46266fc
Aahadsiddig <aahadsiddigo@gmail.com>
2023-09-14 08:24:32 -0700

    done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,11 @@
-#ifndef VARIADIC_H
-#define VARIADIC_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct token - struct token
- * @token: format token
- * @f: the function associated
- */
-
-typedef struct token
-{
-	char *token;
-	void (*f)(char *, va_list);
-} token_t;
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
